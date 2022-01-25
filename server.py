39855c85d17967ce46483bf6dd57d8a0f7ef5897
Miller MA <this.is.millerma@gmail.com>
2022-01-24 23:21:16 -0700

    server update

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 #  coding: utf-8 
 import socketserver
+import os
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Ruilin Ma
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,9 +31,60 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+        #received request and decode the message
         self.data = self.request.recv(1024).strip()
+        decodeMsg = self.data.decode().split()
+        # print ("decodeMsg is: ", decodeMsg[1].strip('/'))
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        #check 404 and 405
+        if decodeMsg == [] or "../" in decodeMsg[1]:
+            # print("HTTP/1.1 404 Not FOUND\r\n")
+            self.request.sendall(bytearray("HTTP/1.1 404 Not FOUND\r\n",'utf-8'))
+        elif decodeMsg[0] != "GET":
+            self.request.sendall(bytearray("HTTP/1.1 405 Method Not ALLOWED\r\n",'utf-8'))
+        
+        #rebuild resource address 
+        newAddress = "./www"+decodeMsg[1]
+        # print ("newAddress is : ", newAddress)
+
+        #check the resourse type and exist (dir or file)
+        #https://www.w3resource.com/python-exercises/python-basic-exercise-85.php
+        if os.path.isdir(newAddress):  
+            
+            if newAddress[-1] == '/':
+                # print("file type is: ", newAddress[-4:])  
+                newAddress += 'index.html'
+                file = open(newAddress, "r")
+                txt = file.read().replace('\n', '')
+                contentType = "text/html"
+                # print("HTTP/1.1 200 OK\r\n Location:" + contentType + "\r\n" + txt +"\r\n")
+                self.request.sendall(bytearray("HTTP/1.1 200 OK\r\nContent-Type:" + contentType + "\r\n" + txt +"\r\n",'utf-8'))
+                file.close()
+            elif newAddress[-1] != '/':
+                # print("HTTP/1.1 301 Moved Permanently Location:" + newAddress + "/" + "\r\n")
+                self.request.sendall(bytearray("HTTP/1.1 301 Moved Permanently Location:" + newAddress + "/" + "\r\n",'utf-8'))  
+                
+        elif os.path.isfile(newAddress):  
+            # print("file type is: ", newAddress[-4:])  
+            file = open(newAddress, "r")
+            #f.readlines()
+            txt = file.read().replace('\n', '')
+            # print ("file is: ", file.read().replace('\n', ''))
+            if newAddress[-4:] == ".css":
+                contentType = "text/css"
+                # print ("HTTP/1.1 200 OK\r\nContent-Type:" + contentType + "\r\n" + txt +"\r\n")
+                self.request.sendall(bytearray("HTTP/1.1 200 OK\r\nContent-Type:" + contentType + "\r\n" + txt +"\r\n",'utf-8'))
+            elif newAddress[-5:] == ".html":
+                contentType = "text/html"
+                self.request.sendall(bytearray("HTTP/1.1 200 OK\r\nContent-Type:" + contentType + "\r\n" + txt +"\r\n",'utf-8'))
+            else:
+                self.request.sendall(bytearray("HTTP/1.1 404 Not FOUND\r\n",'utf-8'))
+            file.close()
+        else:
+            # print("HTTP/1.1 404 Not FOUND\r\n")
+            # print("result is: ",os.path.isdir('./www/deep/deep/'))
+            self.request.sendall(bytearray("HTTP/1.1 404 Not FOUND\r\n",'utf-8'))
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
